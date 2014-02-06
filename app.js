476c06abc3eb43a30ff5ddcbe933af64caefe2a1
Nick Thompson <nickt@instagram.com>
2014-02-06 15:29:55 -0800

    Synchronous bundle exec step

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var http = require('http');
 var path = require('path');
 var app = express();
 var bundleRequire = require('./lib/bundleRequire');
+var serverRoot = bundleRequire(
+  './static/scripts/bundles/server.js',
+  'serverRoot'
+);
 
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -14,28 +18,35 @@ app.use(express.methodOverride());
 app.use(express.errorHandler());
 app.use('/static', express.static(path.join(__dirname, 'static')));
 
+// I'm using a little middleware here to prepare the raw queryString for
+// our router. The router will have to do it's own queryString parsing because
+// there's no nice middleware for that on the client. Thus, to remain truly
+// idempotent, I'll have the router parse the raw queryString on the server
+// as well.
 app.use(function(req, res, next) {
-  var data = {};
   var i = req.originalUrl.indexOf('?');
-  var qs = (i > -1)
+  var queryString = (i > -1)
     ? req.originalUrl.slice(i)
     : null;
 
-  bundleRequire('./static/scripts/bundles/server.js', function(r) {
-    var serverRoot = r('serverRoot');
-    var ua = req.get('User-Agent');
-    if (/Chrome/.test(ua)) {
-      // Let's client-render in chrome just for giggles
-      serverRoot.clientRender(req.path, qs, data, function(markup) {
-        res.send(200, markup);
-      });
-    } else {
-      // And server-render elsewhere
-      serverRoot.serverRender(req.path, qs, data, function(markup) {
-        res.send(200, markup);
-      });
-    }
-  });
+  req.queryString = queryString;
+  return next();
+});
+
+app.use(function(req, res, next) {
+  var data = {};
+  var ua = req.get('User-Agent');
+  if (/Chrome/.test(ua)) {
+    // Let's client-render in chrome just for giggles
+    serverRoot.clientRender(req.path, req.queryString, data, function(markup) {
+      res.send(200, markup);
+    });
+  } else {
+    // And server-render elsewhere
+    serverRoot.serverRender(req.path, req.queryString, data, function(markup) {
+      res.send(200, markup);
+    });
+  }
 });
 
 http.createServer(app).listen(3000, function() {
